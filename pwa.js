f56297b3f5324101987e002384c379801a9de7be
Barry <8173barry@gmail.com>
2025-08-11 00:19:09 +0800

    Update PWA

--- pwa.js
+++ pwa.js
@@ -1,19 +1,8 @@
-// pwa.js
 (function () {
-  if (!('serviceWorker' in navigator)) {
-    console.warn('[PWA] Service Worker not supported');
-    return;
-  }
+  if (!('serviceWorker' in navigator)) return;
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('./service-worker.js')
-      .then(reg => {
-        console.log('[PWA] SW registered with scope:', reg.scope);
-        reg.onupdatefound = () => {
-          const nw = reg.installing;
-          if (!nw) return;
-          nw.onstatechange = () => console.log('[PWA] SW state:', nw.state);
-        };
-      })
-      .catch(err => console.error('[PWA] SW register failed:', err));
+      .then(reg => console.log('[PWA] scope:', reg.scope))
+      .catch(err => console.error('[PWA] register failed:', err));
   });
 })();
